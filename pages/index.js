c3125b472d16fcecd378b994e0650b204c9851b7
Israel Sotomayor <israel.sotomayor@eyetive.com>
2019-08-05 16:37:35 -0500

    Using Shared Components

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Link from 'next/link';
+import Header from '../comps/Header';
 
 const Index = () => (
   <div>
-  <Link href="/about" >
-    <a title="About Page">About Page</a>
-  </Link>
-  <p>Hello Next.js</p>
+    <Header />
+    <p>Hello Next.js</p>
   </div>
 );
 
