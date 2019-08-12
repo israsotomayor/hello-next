9d46ba9c62c25b2a3defd9ec3b8973b536710380
Israel Sotomayor <israel.sotomayor@eyetive.com>
2019-08-12 16:57:20 -0500

    Clean URLs

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,35 @@
 import Layout from '../components/MyLayout';
 import withLayout from '../components/MyLayout';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
 
 /*export default function Index() {
   return (
@@ -24,15 +53,23 @@ export default function Index() {
   return <Layout content={indexPageContent} />;
 }*/
 
-const PostLink = props => (
+/*const PostLink = props => (
   <li>
     <Link href={`/post?title=${props.title}`}>
       <a>{props.title}</a>
     </Link>
   </li>
-);
+);*/
 
-export default function Blog() {
+/*const PostLink = props => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${props.id}`}>
+      <a>{props.id}</a>
+    </Link>
+  </li>
+);*/
+
+/*export default function Blog() {
   return (
     <Layout>
       <h1>My Blog</h1>
@@ -42,5 +79,19 @@ export default function Blog() {
         <PostLink title="Deploy apps with Zeit" />
       </ul>
     </Layout>
-  )
-}
+  );
+}*/
+
+/*export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink id="hello-nextjs" />
+        <PostLink id="learn-nextjs" />
+        <PostLink id="deploy-nextjs" />
+      </ul>
+    </Layout>
+  );
+}*/
+
